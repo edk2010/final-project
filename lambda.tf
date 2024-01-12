cf6f8d3bc4d43232cd4b1737cb76748f4762bedb
edk <lnage2@gmail.com>
2024-01-13 00:13:52 +0200

    terrraform+jenkins

--- lambda.tf
+++ lambda.tf
@@ -19,9 +19,9 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 */
 
-data "archive_file" "lambda" {
+data "archive_file" "mylambda" {
   type        = "zip"
-  source_file = "./lambda.py"
+  source_file = "./mylambda.py"
   output_path = "lambda_function_payload.zip"
 }
 
@@ -36,9 +36,9 @@ resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
   role          = data.aws_iam_role.lambda_rw_s3.arn
-  handler       = "lambda.lambda_handler"
+  handler       = "mylambda.lambda_handler"
 
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  source_code_hash = data.archive_file.mylambda.output_base64sha256
 
   runtime = "python3.10"
 
