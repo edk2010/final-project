a1caa0896844ba2b0942e5984d8d6a26fe07c634
edk <lnage2@gmail.com>
2024-01-14 22:50:08 +0200

    unitest

--- unitest_api.py
+++ unitest_api.py
@@ -1,29 +1,23 @@
 import requests
 import unittest
-import os
 import sys
-import argparse
 
 class APITestCase(unittest.TestCase):
 
-    #@classmethod
-    #def setUpClass(cls):
-    #    #cls.api_url = os.environ.get('API_URL')
-    #    cls.api_url = sys.argv[1]
+    @classmethod
+    def setUpClass(cls):
+        cls.api_url = API_URL
 
     def test_get_request(self):
         """Test GET request to a sample API endpoint"""
-        #response = requests.get(f'{self.api_url}')
-        response = requests.get(f'{API_URL}')
+        response = requests.get(self.api_url)
         self.assertEqual(response.status_code, 200)
         # Additional assertions can be added here
 
-# Below code ensures argparse is used only when running directly
 if __name__ == "__main__":
-    
-    #parser = argparse.ArgumentParser(description="API Test")
-    #parser.add_argument('api_url', type=str, help='URL of the API to test')
-    #args = parser.parse_args()
-    API_URL = sys.argv[1]
-    #os.environ['API_URL'] = args.api_url
+    if len(sys.argv) > 1:
+        API_URL = sys.argv.pop(1)  # Pop the URL argument and assign it
+    else:
+        raise ValueError("No URL provided as a command-line argument")
+
     unittest.main()
