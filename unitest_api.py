33d1b654d431451b4974261d5a5f1ff08e2c82d8
edk <lnage2@gmail.com>
2024-01-14 22:40:35 +0200

    unitest

--- unitest_api.py
+++ unitest_api.py
@@ -2,14 +2,15 @@ import requests
 import unittest
 import os
 import sys
+import argparse
 
 class APITestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.api_url = os.environ.get('API_URL')
+        #cls.api_url = os.environ.get('API_URL')
         cls.api_url = sys.argv[1]
-        
+
     def test_get_request(self):
         """Test GET request to a sample API endpoint"""
         response = requests.get(f'{self.api_url}')
@@ -18,10 +19,10 @@ class APITestCase(unittest.TestCase):
 
 # Below code ensures argparse is used only when running directly
 if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser(description="API Test")
-    parser.add_argument('api_url', type=str, help='URL of the API to test')
-    args = parser.parse_args()
-    API_URL = sys.argv[1]
+    
+    #parser = argparse.ArgumentParser(description="API Test")
+    #parser.add_argument('api_url', type=str, help='URL of the API to test')
+    #args = parser.parse_args()
+    #API_URL = sys.argv[1]
     #os.environ['API_URL'] = args.api_url
     unittest.main()
