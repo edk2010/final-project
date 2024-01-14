be55cc9b3636508a65bcf4f9b7803d81401dfcef
edk <lnage2@gmail.com>
2024-01-14 22:46:13 +0200

    unitest

--- unitest_api.py
+++ unitest_api.py
@@ -6,14 +6,15 @@ import argparse
 
 class APITestCase(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        #cls.api_url = os.environ.get('API_URL')
-        cls.api_url = sys.argv[1]
+    #@classmethod
+    #def setUpClass(cls):
+    #    #cls.api_url = os.environ.get('API_URL')
+    #    cls.api_url = sys.argv[1]
 
     def test_get_request(self):
         """Test GET request to a sample API endpoint"""
-        response = requests.get(f'{self.api_url}')
+        #response = requests.get(f'{self.api_url}')
+        response = requests.get(f'{API_URL}')
         self.assertEqual(response.status_code, 200)
         # Additional assertions can be added here
 
@@ -23,6 +24,6 @@ if __name__ == "__main__":
     #parser = argparse.ArgumentParser(description="API Test")
     #parser.add_argument('api_url', type=str, help='URL of the API to test')
     #args = parser.parse_args()
-    #API_URL = sys.argv[1]
+    API_URL = sys.argv[1]
     #os.environ['API_URL'] = args.api_url
     unittest.main()
